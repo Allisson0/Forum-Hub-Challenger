83b0f4814cbfdf09bbb0d8eccb750195aa7be82a
Allisson0 <allissonhiaggo@gmail.com>
2026-02-13 20:01:12 -0300

    feat(SecurityConfigurations): adicionando filtro do desenvolvedor antes do Spring

--- src/main/java/br/com/forum/api/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/forum/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.forum.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter filtroDeSeguranca;
 
     // ==== CONFIGURAÇÕES DE SEGURANÇA ====
     @Bean
@@ -30,6 +35,8 @@ public class SecurityConfigurations {
                     // Acesso ao restante, apenas autenticado
                     req.anyRequest().authenticated();
                 })
+                // Adicionando nosso filtro de segurança, antes do de autenticação do Spring
+                .addFilterBefore(filtroDeSeguranca, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
