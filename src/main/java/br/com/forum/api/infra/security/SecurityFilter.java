908cd373bba7e3f4171cb6c50f68ea2e21891c15
Allisson0 <allissonhiaggo@gmail.com>
2026-02-13 19:58:44 -0300

    feat(SecurityFilter): assegurando autenticação via token valido

--- src/main/java/br/com/forum/api/infra/security/SecurityFilter.java
+++ src/main/java/br/com/forum/api/infra/security/SecurityFilter.java
@@ -1,11 +1,13 @@
 package br.com.forum.api.infra.security;
 
-import br.com.forum.api.domain.ValidacaoException;
+import br.com.forum.api.domain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,6 +20,9 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     // ==== PARA CADA REQUISIÇÃO FAÇA ESTE FILTRO ====
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
@@ -26,8 +31,28 @@ public class SecurityFilter extends OncePerRequestFilter {
         // Recupera o token, a partir da requisição
         var tokenJwt = recuperarToken(request);
 
-        // Valida este token e recupera seu usuário
-        var usuario = tokenService.validarToken(tokenJwt);
+        // SE houver token, faça:
+        if (tokenJwt!= null) {
+            // Valida este token e recupera seu usuário
+            var subject = tokenService.validarToken(tokenJwt);
+
+            // Como o Email é o login do usuário, procura este
+            // login (considerando que nosso token obrigatoriamente
+            // necessita haver um, para ser válido)
+            var usuario = usuarioRepository.findByEmail(subject);
+
+            // Como não há necessidade de credenciais neste projeto, criamos
+            // uma autenticação com credenciais de valor nulo
+            var authentication = new UsernamePasswordAuthenticationToken(
+                    usuario, null, usuario.getAuthorities());
+
+            // Altera o contexto para 'autenticado'
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        // Se não, o Spring Security valida se é uma requisição de tipo
+        // a não precisar de autenticação (ex.: POST /login)
+
 
         // Chama o próximo filtro da requisição
         filterChain.doFilter(request, response);
@@ -40,12 +65,13 @@ public class SecurityFilter extends OncePerRequestFilter {
         // Recupera o token do cabeçalho de authorization
         var cabecalhoDeAutorizacao = http.getHeader("Authorization");
 
-        // SE não vier nada, chama uma exception de validação
-        if (cabecalhoDeAutorizacao == null) {
-            throw new ValidacaoException("Token JWT não enviado no cabeçalho de autorização.");
+        // SE for diferente de null, retorna o String removendo o "Bearer "
+        // do token
+        if (cabecalhoDeAutorizacao != null) {
+            return cabecalhoDeAutorizacao.replace("Bearer ", "");
         }
 
-        // Se houver, retorna ele
-        return cabecalhoDeAutorizacao;
+        // Se não houver nada, retorna nulo
+        return null;
     }
 }
