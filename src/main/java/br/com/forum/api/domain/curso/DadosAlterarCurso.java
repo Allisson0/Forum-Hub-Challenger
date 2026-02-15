e92ea45a6ae09314d3219d930a26ca329471be32
Allisson0 <allissonhiaggo@gmail.com>
2026-02-15 15:32:49 -0300

    feat(DadosAlterarCurso): validação notNull correta para long id

--- src/main/java/br/com/forum/api/domain/curso/DadosAlterarCurso.java
+++ src/main/java/br/com/forum/api/domain/curso/DadosAlterarCurso.java
@@ -1,9 +1,10 @@
 package br.com.forum.api.domain.curso;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record DadosAlterarCurso(
-        @NotBlank
+        @NotNull
         Long id,
 
         @NotBlank
